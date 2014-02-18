66195b8c61e9837a652e0cf3b51385a6bc66057a
Phillip Villarosa <ppvillarosa@gmail.com>
2014-02-18 22:02:27 +0800

    added jquery, bootstrap, and setup of home module

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,14 +1,26 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html lang="">
   <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <?php include_http_metas() ?>
     <?php include_metas() ?>
-    <?php include_title() ?>
+    <title><?php echo get_slot('page_title', 'ManilaCoders.com') ?></title>
     <link rel="shortcut icon" href="/favicon.ico" />
-    <?php include_stylesheets() ?>
+
+    <link href="<?php echo stylesheet_path('bootstrap.min.css') ?>" rel="stylesheet">
+    <!-- include css -->
+    <?php include_slot('page_css') ?>
+
+    <!-- jquery -->
+    <script src="<?php echo javascript_path('jquery.min.js') ?>"></script>
+    
     <?php include_javascripts() ?>
   </head>
   <body>
     <?php echo $sf_content ?>
+    
+    <script src="<?php echo javascript_path('bootstrap.min.js') ?>"></script>
   </body>
 </html>
